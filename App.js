efd1f546adf13ffabd9e2d6fe571db06a1df58aa
Three <ottunade95@gmail.com>
2025-07-05 16:56:41 +0100

    Add navigation setup and dependencies for React Navigation

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+  const BottomTabs = createBottomTabNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{
+              headerStyle: { backgroundColor: "#351401" },
+              headerTintColor: "white",
+              contentStyle: { backgroundColor: "#3f2f25" },
+            }}
+          >
+            <Stack.Screen name="MainScreen" component={MainScreen} />
+
+            <Stack.Screen
+              name="ManageScreen"
+              component={ManageExercise}
+              //options={{ presentation: "modal" }}
+              // the presentation option is used to show the screen as a modal which only works on iOS
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
